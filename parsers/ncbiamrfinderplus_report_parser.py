1f5a7604feea86cd05b3a33fa76a0ed2d5f1896b
Dan Fornika <dfornika@gmail.com>
2020-03-27 13:32:57 -0700

    Map amrfinderplus fields to harmonized fields

--- parsers/ncbiamrfinderplus_report_parser.py
+++ parsers/ncbiamrfinderplus_report_parser.py
@@ -7,28 +7,28 @@ import json
 from AntimicrobialResistance.Result import AntimicrobialResistanceResult
 
 FIELD_MAP_NCBIAMRFINDERPLUS = {
-    'protein_identifier': '',
-    'contig_id': '',
-    'start': '',
-    'stop': '',
-    'strand': '',
-    'gene_symbol': '',
-    'sequence_name': '',
-    'scope': '',
-    'element_type': '',
-    'element_subtype': '',
-    'class': '',
-    'subclass': '',
-    'method': '',
-    'target_length': '',
-    'reference_sequence_length': '',
-    'percent_coverage_of_reference_sequence': '',
-    'percent_identity_of_reference_sequence': '',
-    'alignment_length': '',
-    'accession_of_closest_sequence': '',
-    'name_of_closest_sequence': '',
-    'hmm_id': '',
-    'hmm_description': '',
+    'protein_identifier': None,
+    'contig_id': 'contig',
+    'start': 'start',
+    'stop': 'stop',
+    'strand': 'strand_orientation',
+    'gene_symbol': 'resistance_gene_symbol',
+    'sequence_name': 'resistance_gene_name',
+    'scope': None,
+    'element_type': None,
+    'element_subtype': None,
+    'class': 'drug_class',
+    'subclass': None,
+    'method': None,
+    'target_length': 'target_length',
+    'reference_sequence_length': None,
+    'percent_coverage_of_reference_sequence': 'coverage_percent',
+    'percent_identity_of_reference_sequence': 'sequence_identity',
+    'alignment_length': None,
+    'accession_of_closest_sequence': 'reference_accession',
+    'name_of_closest_sequence': None,
+    'hmm_id': None,
+    'hmm_description': None,
 }
 
 def parse_ncbi_amrfinderplus_report(path_to_ncbi_amrfinderplus_report):
@@ -107,11 +107,15 @@ def parse_ncbi_amrfinderplus_report(path_to_ncbi_amrfinderplus_report):
     return ncbi_amrfinderplus_report
 
 
-def prepare_for_amr_class(parsed_ncbi_amrfinderplus_report):
+def prepare_for_amr_class(parsed_ncbi_amrfinderplus_report, additional_fields={}):
     input_for_amr_class = {}
-    
+    input_for_amr_class['analysis_software_name'] = "AMRFinderPlus"
+    for key, value in additional_fields:
+        input_for_amr_class[key] = value
+
     for ncbi_amrfinderplus_field, amr_result_field in FIELD_MAP_NCBIAMRFINDERPLUS.items():
-        input_for_amr_class[str(amr_result_field)] = parsed_ncbi_amrfinderplus_report[str(ncbi_amrfinderplus_field)]
+        if amr_result_field:
+            input_for_amr_class[str(amr_result_field)] = parsed_ncbi_amrfinderplus_report[str(ncbi_amrfinderplus_field)]
 
     return input_for_amr_class
 
@@ -119,15 +123,24 @@ def prepare_for_amr_class(parsed_ncbi_amrfinderplus_report):
 def main(args):
     parsed_ncbi_amrfinderplus_report = parse_ncbi_amrfinderplus_report(args.ncbi_amrfinderplus_report)
 
-    for result in parsed_ncbi_amrfinderplus_report:
-        amr_class_input = prepare_for_amr_class(result)
-        amr_result = AntimicrobialResistanceResult(amr_class_input)
-        #print(amr_result)
+    additional_fields = {}
+    if args.analysis_software_version:
+        additional_fields['analysis_software_version'] = args.analysis_software_version
+    if args.database_version:
+        additional_fields['database_version'] = args.database_version
 
-    print(json.dumps(parsed_ncbi_amrfinderplus_report))
+    amr_results = []
+    for result in parsed_ncbi_amrfinderplus_report:
+        amr_class_input = prepare_for_amr_class(result, additional_fields)
+        amr_result = AntimicrobialResistanceResult(amr_class_input)
+        amr_results.append(amr_result)
+
+    print(amr_results)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("ncbi_amrfinderplus_report", help="Input NCBI AMRFinderPlus report")
+    parser.add_argument("--analysis_software_version", help="Version of NCBI AMRFinderPlus used to generate the report")
+    parser.add_argument("--database_version", help="Version of NCBI AMRFinder database used to generate the report")
     args = parser.parse_args()
     main(args)
