49dfa4608001030d47d6f4dcac81e0e8b786bc05
Finlay Maguire <finlaymaguire@gmail.com>
2020-11-04 11:09:05 -0400

    Add interactive summary prototype

--- hAMRonization/summarize.py
+++ hAMRonization/summarize.py
@@ -6,13 +6,219 @@ import pandas as pd
 import os
 import sys
 import json
+from string import Template
+
+def format_interactive_json(combined_records):
+    """
+    Reorganised json to row/table format for ease of display
+    """
+    # dummy field to handle multiple configs
+    combined_records['config'] = combined_records['analysis_software_name'].astype(str) +\
+        combined_records['analysis_software_version'].astype(str) +\
+        combined_records['reference_database_id'].astype(str) +\
+        combined_records['reference_database_version'].astype(str)
+
+    configs = combined_records[['config',
+                  'analysis_software_name',
+                  'analysis_software_version',
+                  'reference_database_id',
+                  'reference_database_version']].drop_duplicates()
+
+
+    tool_groups = configs.groupby('analysis_software_name')
+    configs['display_name'] = configs['analysis_software_name'] + \
+                                    tool_groups.cumcount().apply(lambda x:f"\n(config {x})")
+
+    config_display_names = configs.set_index('config')['display_name'].to_dict()
+    combined_records['config_display_name'] = combined_records['config'].apply(lambda x: config_display_names[x])
+
+    combined_records = combined_records.drop('config', axis=1)
+
+
+    data_for_summary = {}
+
+    grouped_data = combined_records.groupby(['input_file_name', 'config_display_name']).apply(lambda x: x.to_json(orient='records'))
+    for (input_file, config), hits in grouped_data.iteritems():
+        json_hits = json.loads(hits)
+        if input_file not in data_for_summary:
+            data_for_summary[input_file] = [{config: json_hits}]
+        else:
+            data_for_summary[input_file].append({config: json_hits})
+
+
+    tidied_json = []
+    for genome in combined_records['input_file_name'].sort_values().unique():
+        genome_data = {'input_file_name': genome}
+        for config_results in data_for_summary[genome]:
+            config = config_results.keys()
+            if len(config) != 1:
+                raise ValueError
+            else:
+                config = list(config)[0]
+            genome_data[config] = config_results[config]
+        tidied_json.append(genome_data)
+
+    tidied_json = json.dumps(tidied_json)
+    return tidied_json
+
 
 def generate_interactive_report(combined_report_data):
     """
     Generate interactive HTML/js report based on what alex sent
     """
+    tidied_json = format_interactive_json(combined_report_data)
 
-    raise NotImplementedError
+    html_template ="""<!DOCTYPE html>
+<html>
+  <head>
+    <title>Example</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+  </head>
+  <body>
+    <div class="container bg-light">
+    <div class="row">
+        <div class="container bg-light" id="dynamic-table">
+    <script type='text/javascript'>
+   //parse JSON
+    var hamronized_data = JSON.parse($json_data)
+
+
+    function CreateTableFromJSON(){
+      //get values for html header
+      // Tool1, Tool2, Tool3
+      var columns = [];
+      for (var i = 0; i < hamronized_data.length; i++){
+        for (var toolconfig in hamronized_data[i]){
+          if (columns.indexOf(toolconfig) === -1){
+            columns.push(toolconfig);
+          }
+        }
+      }
+
+     //Create Table
+      var table = document.createElement("table");
+
+      //make table header row
+      var tr = table.insertRow(-1);
+      for (var i = 0; i < columns.length; i++){
+        var th = document.createElement("th"); //Header
+        // skip putting the input_file_name as the row label header
+        if (i == 0) {
+            th.innerHTML = '';
+        } else {
+            th.innerHTML = columns[i];
+        }
+        tr.appendChild(th);
+      }
+
+      // Add other data as Rows
+      for (var i = 0; i < hamronized_data.length; i++){
+        tr = table.insertRow(-1);
+
+		// for each row add the data to the appropriate column
+        for (var j=0; j<columns.length; j++){
+          var tableCell = tr.insertCell(-1);
+          var entry = hamronized_data[i][columns[j]];
+
+          //If type is object, we make a list for the entry
+          // This is the "list of AMR hits"
+
+          if(typeof entry === 'object' && entry !== null){
+            //create a list
+            var list = document.createElement("ul");
+            list.setAttribute("class", "list-group");
+            //for each amr hit, put the gene_symbol in the list
+            for (var k=0; k < entry.length; k++){
+              // we can use html5 data-attributes to store data on the cells.
+              var listBullet = document.createElement('li');
+
+              listBullet.setAttribute("class", "list-group-item amr_hit");
+              // we can use html5 data-attributes to store data on the cells.
+              // The data must be type string.
+
+              var amrData = JSON.stringify(entry[k]);
+              listBullet.setAttribute("hamronized_result", amrData);
+              listBullet.innerHTML = entry[k].gene_symbol;
+              list.appendChild(listBullet);
+            }
+            tableCell.innerHTML = list.outerHTML;
+          }
+          //otherwise, the entry is the input file name
+          else{
+            tableCell.innerHTML = entry;
+            tableCell.setAttribute("class", "input_file_name");
+          }
+        }
+      }
+      //Add the table to a container div
+      var divContainer = document.getElementById("dynamic-table");
+      divContainer.innerHTML = "";
+      divContainer.appendChild(table);
+
+      //Now that the table exists, add OnClick listeners to all the dna-gene class objects
+      var geneElements = document.getElementsByClassName('amr_hit');
+      for (var i = 0; i < geneElements.length; i++){
+        geneElements[i].addEventListener('click', displayAmrData, false);
+      }
+    }
+
+    //function to display gene data
+    function displayAmrData(){
+      amr_data = JSON.parse(this.getAttribute('hamronized_result'));
+      var list = document.createElement("ul");
+      list.setAttribute("class", "list-group-flush");
+
+
+      //retrieve the data, format it as strings
+      for (var field in amr_data){
+        var data = amr_data[field];
+		if (data !== null){
+        	var output = "";
+			output = output.concat(field, ": ", data);
+
+        	//add an entry to the list
+        	var listBullet = document.createElement('li');
+        	listBullet.setAttribute("class", "list-group-item");
+        	listBullet.innerHTML = output;
+        	list.appendChild(listBullet);
+		}
+      }
+      //display the list
+      var divContainer = document.getElementById('data-display');
+      divContainer.innerHTML = "";
+      divContainer.appendChild(list);
+    }
+
+
+        CreateTableFromJSON()
+        </script>
+          <!--<input type='button' onclick='CreateTableFromJSON()' value='make table'/>-->
+      </div>
+    </div>
+    <div class="row">
+      <div class="container bg-light" id="data-display">
+      </div>
+    </div>
+  </div>
+
+  </body>
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</html>"""
+
+    html_template = Template(html_template)
+
+    interactive_report = html_template.substitute(json_data=tidied_json)
+
+    return interactive_report
 
 
 def check_report_type(file_path):
