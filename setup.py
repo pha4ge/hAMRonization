83c03aada1f8fb02c91eccfaf4f54475e06be4f8
Finlay Maguire <finlaymaguire@gmail.com>
2021-02-01 21:10:49 -0400

    Add extra setup data for PyPI and conda packaging

--- setup.py
+++ setup.py
@@ -12,19 +12,41 @@ with open('hAMRonization/__init__.py') as fh:
 
 setup(
     name='hAMRonization',
+    packages=['hAMRonization'],
     version=version,
-    author=['Finlay Maguire', 'Dan Fornika'],
-    python_requires='>=3.7',
+    license="LGPLv3",
+    description="Tool to convert and summarize AMR gene detection outputs "
+                "using the hAMRonization specification",
+    author='Finlay Maguire',
     author_email='finlaymaguire@gmail.com',
-    description='hAMRonization AMR gene detection implementation',
+    url="https://github.com/pha4ge/hAMRonization",
+    download_url=f"https://github.com/pha4ge/archive/v{version}.tar.gz",
+    keywords=["Genomics", "Antimicrobial resistance", "Antibiotic",
+              "Standardization"],
+    python_requires='>=3.7',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pha4ge/hAMRonization",
     install_requires=['pandas'],
     entry_points={
         'console_scripts': [
             'hamronize = hAMRonization.hamronize:main'
             ],
         },
-    packages=['hAMRonization'],
-)
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: POSIX",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: Healthcare Industry",
+        "Topic :: Scientific/Engineering :: Bio-Informatics"
+        ],
+    zip_safe=True,
+    )
